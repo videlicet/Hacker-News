16799c6b7ee8f2813b3514c9bc8b8509c74806f6
Christoph Bornemann <bornemch@gmail.com>
2023-03-20 18:13:16 +0100

    Initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+
+// interface Data {
+//   hits: [{
+//     title: string
+//   }]
+// }
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  // const fetchJson = () => {
+  //   fetch('hackernews.json')
+  //   // .then((response) => {
+  //   //   if (!response.ok) 
+  //   //     throw new Error(`Request failed with a status of ${response.status}`);
+  //   //     return response.json();
+  //   // })
+  //   // .then((news) => {
+  //   //   setNews(news);
+  //   // })
+  //   // .catch(error => console.log(error.message));
+  //   .then(response => {
+  //     return response.json();
+  //   }).then(newsItem => {
+  //     setNews(newsItem);
+  //   }).catch(error => console.log(error.message));
+  // };
+
+  const getData=()=>{
+    fetch('./hackernews.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setNews(myJson);
+      });
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header className='header'>
+        <h1>Hacker News</h1>
+        <form>
+          <input type='text'/>
+          <input type='submit' value=''/>
+        </form>
       </header>
+      <div className='results'>
+        {
+          news && Object.keys(news).length > 0 && news.hits.map((newsItem) =>
+          <div className='news-item'>{newsItem.title}</div>)
+        }
+      </div>
     </div>
   );
 }
